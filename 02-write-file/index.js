99f38076dcdfaf38bebde72cb9e593d11a2eb469
Yauhen <amidawolf@gmail.com>
2021-11-05 20:43:38 +0300

    feat: add solution for 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const file = path.join(__dirname, 'consoleText.txt');
+
+//создаём файл и уже после создания начинаем с ним работать
+fs.writeFile(file, '', err => {
+  if (err) {
+    throw err;
+  }
+
+  let welcomeText = 'Please enter text: ';
+  let exitText = 'Press "ctrl+c" or write "exit"';
+
+  //создаём интерфейс для ввода/вывода
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  //выводим приветствие
+  rl.output.write(exitText + '\n' + welcomeText);
+
+  //вешаем обработчик на строку ввода
+  rl.on('line', input => {
+    if (input === 'exit') {
+      rl.close();
+      return;
+    }
+
+    //добавляем к файлу строку ввода с переносом в конце
+    fs.appendFile(file, input + '\n', err => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+
+  //вешаем обработчик на завершение работы для вывода прощального текста
+  rl.on('close', () => {
+    return console.log('Writing completed');
+  });
+});
